86d84fd85dd22c36d51c10ab632f7856070c508f
Paul Lazo <paullazo19@gmail.com>
2016-05-01 00:27:01 -0500

    added delete and item counter functionality to todo list app

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 $(function(){
 
   //variables
-  var $itemInput = $("[data-js='todo_item']");
+  var $todoInput = $("[data-js='todo_input']");
   var $todoForm = $("[data-js='todo_form']");
   var $todoLog = $("[data-js='todo_log']");
+  var $itemCount = $("[data-js='item_counter']");
+  var $selectedItem = [];
+
 
   //prevent default on submit
   $todoForm.on("submit", function(e){
@@ -13,23 +16,55 @@ $(function(){
   // blank todo item with a placeholder of "type todo item here"
   // type more than 2 characters then return key to create the item
 
-  $($itemInput).on("keyup", function(e){
-    // var $elText = $(e.target);
-    if (e.keyCode === 13) {
-      console.log("Bakon!");
-    };
-
-    if (e.target.value.length >= 2) {
-      // console.log(e.target.val.length);
+  $todoInput.on("keyup", function(e){
+    var $elText = $(e.target);
+    $todolog = [];
+    $selectedItem = [];
+    $itemCount = 0;
+    if (e.keyCode === 13 && $elText.val().length > 1) {
       $todoLog.prepend(`
-          <li class="todo_item">
+
+          <li class="todo__item" data-js="todo_item">
+            <span class="item__marker"></span>
             ${$elText.val()}
           </li>
-        `);
-        //Clear value after
-        $elText.val("");
-    }
-    // e.preventDefault();
-  });
+      `);
 
+      //Clear value after
+      $elText.val("");
+
+      var $count = $todoLog.children().length;
+      // console.log($count);
+      // $itemCount = $count;
+
+
+      var $todoItem = $("[data-js='todo_item']");
+
+      $todoItem.on("click", function(e){
+        $selectedItem = $(e.target);
+      });
+    }
+  })
+
+  // number of how many items are left in the todo list
+  // delete a todo when selected with one click and press the delete key
+  $todoInput.on("keyup", function(e){
+    var $itemCount = $("[data-js='item_counter']");
+    if (e.keyCode === 13) {
+
+      var $count = $todoLog.children().length;
+      $itemCount.html($count);
+
+      var $body = $("[data-js='body']");
+      $body.on("keyup", function(e){
+        var $count = $todoLog.children().length;
+        if (e.keyCode === 8 && $selectedItem.length > 0) {
+
+          $selectedItem.remove();
+          $itemCount.html($count);
+        }
+      });
+    }
+
+  });
 });
